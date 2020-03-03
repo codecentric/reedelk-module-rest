f26f148958c52068473316dc3108ebff84f40ec3
lorenzo <lorenzo@lorenzo.info.com>
2020-03-03 12:10:07 +0100

    code

--- src/main/java/com/reedelk/rest/script/MultipartPartBuilder.java
+++ src/main/java/com/reedelk/rest/script/MultipartPartBuilder.java
@@ -4,7 +4,9 @@ import com.reedelk.runtime.api.annotation.AutocompleteItem;
 import com.reedelk.runtime.api.annotation.AutocompleteType;
 import com.reedelk.runtime.api.message.content.*;
 
-@AutocompleteType(description = "Multipart Part builder")
+@AutocompleteType(
+        description = "The MultipartPartBuilder allows to build a REST Part object. " +
+                "The MultipartPartBuilder type is used together wihth MultipartBuilder to build REST Multipart payloads.")
 public class MultipartPartBuilder {
 
     private final MultipartPartsBuilder parent;
@@ -15,27 +17,42 @@ public class MultipartPartBuilder {
         current = Part.builder().name(partName);
     }
 
-    @AutocompleteItem(signature = "attribute('','')", cursorOffset = 5, description = "Adds a new part with the given name")
+    @AutocompleteItem(
+            cursorOffset = 1,
+            signature = "attribute(key: String, value: String)",
+            example = "MultipartBuilder.part('binaryContent').attribute('filename','my_image.png')",
+            description = "Adds a new attribute with the given key and value to the part object.")
     public MultipartPartBuilder attribute(String key, String value) {
         current.attribute(key, value);
         return this;
     }
 
-    @AutocompleteItem(signature = "binary()", cursorOffset = 1, description = "Adds a new part with the given name")
+    @AutocompleteItem(
+            signature = "binary(data: byte[])",
+            example = "MultipartBuilder.part('binaryContent').binary(message.payload())",
+            description = "Sets binary data to the current part object. Default mime type is 'application/octet-stream'.")
     public MultipartPartBuilder binary(byte[] data) {
         ByteArrayContent content = new ByteArrayContent(data, MimeType.APPLICATION_BINARY);
         current.content(content);
         return this;
     }
 
-    @AutocompleteItem(signature = "text('')", cursorOffset = 2, description = "Adds a new part with the given name")
+    @AutocompleteItem(
+            cursorOffset = 1,
+            signature = "text(data: String)",
+            example = "MultipartBuilder.part('textContent').text(message.payload())",
+            description = "Sets text data to the current part object. Default mime type is 'text/plain'.")
     public MultipartPartBuilder text(String text) {
         StringContent content = new StringContent(text, MimeType.TEXT);
         current.content(content);
         return this;
     }
 
-    @AutocompleteItem(signature = "binaryWithMimeType(,'" + MimeType.MIME_TYPE_APPLICATION_BINARY + "')", cursorOffset = 28, description = "Adds a new part with the given name")
+    @AutocompleteItem(
+            cursorOffset = 1,
+            signature = "binaryWithMimeType(data: byte[], mimeType: String)",
+            example = "MultipartBuilder.part('binaryContent').binaryWithMimeType(message.payload(), 'application/octet-stream')",
+            description = "Sets binary data to the current part object with the given mime type.")
     public MultipartPartBuilder binaryWithMimeType(byte[] data, String mimeType) {
         MimeType mimeTypeObject = MimeType.parse(mimeType);
         ByteArrayContent content = new ByteArrayContent(data, mimeTypeObject);
@@ -43,7 +60,11 @@ public class MultipartPartBuilder {
         return this;
     }
 
-    @AutocompleteItem(signature = "textWithMimeType('','text/plain')", cursorOffset = 15, description = "Adds a new part with the given name")
+    @AutocompleteItem(
+            cursorOffset = 1,
+            signature = "textWithMimeType(data: String, mimeType: String)",
+            example = "MultipartBuilder.part('textContent').textWithMimeType(message.payload(), 'text/plain')",
+            description = "Sets text data to the current part object with the given mime type.")
     public MultipartPartBuilder textWithMimeType(String text, String mimeType) {
         MimeType mimeTypeObject = MimeType.parse(mimeType);
         StringContent content = new StringContent(text, mimeTypeObject);
@@ -51,13 +72,20 @@ public class MultipartPartBuilder {
         return this;
     }
 
-    @AutocompleteItem(signature = "part('')", cursorOffset = 2, description = "Adds the current")
+    @AutocompleteItem(
+            cursorOffset = 1,
+            signature = "part(partName: String)",
+            example = "MultipartBuilder.part('textContent').text('sample text').part('binaryContent').binary(message.payload()).build()",
+            description = "Adds a new part to the Multipart payload with the given partName.")
     public MultipartPartBuilder part(String partName) {
         parent.add(current.build());
         return parent.part(partName);
     }
 
-    @AutocompleteItem(signature = "build()", description = "Adds a new part with the given name")
+    @AutocompleteItem(
+            signature = "build()",
+            example = "MultipartBuilder.part('textContent').text('sample text').build()",
+            description = "Creates the Multipart object with all the configured parts.")
     public Parts build() {
         return parent.add(current.build()).build();
     }
--- src/main/java/com/reedelk/rest/script/MultipartPartsBuilder.java
+++ src/main/java/com/reedelk/rest/script/MultipartPartsBuilder.java
@@ -5,7 +5,7 @@ import com.reedelk.runtime.api.annotation.AutocompleteType;
 import com.reedelk.runtime.api.message.content.Part;
 import com.reedelk.runtime.api.message.content.Parts;
 
-@AutocompleteType(description = "Multipart parts builder")
+@AutocompleteType(description = "Multipart parts builder allows to create multiple parts")
 public class MultipartPartsBuilder {
 
     private final Parts parts;
@@ -14,12 +14,19 @@ public class MultipartPartsBuilder {
         parts = new Parts();
     }
 
-    @AutocompleteItem(signature = "part('')", cursorOffset = 2, description = "Adds a new part with the given name")
+    @AutocompleteItem(
+            cursorOffset = 1,
+            signature = "part(partName: String)",
+            example = "MultipartBuilder.part('textContent').text('sample text').part('binaryContent').binary(message.payload()).build()",
+            description = "Adds a new part to the Multipart payload with the given partName.")
     public MultipartPartBuilder part(String partName) {
         return new MultipartPartBuilder(this, partName);
     }
 
-    @AutocompleteItem(signature = "build()", cursorOffset = 1, description = "Adds a new part with the given name")
+    @AutocompleteItem(
+            signature = "build()",
+            example = "MultipartBuilder.part('textContent').text('sample text').build()",
+            description = "Creates the Multipart object with all the configured parts.")
     public Parts build() {
         return parts;
     }
