0266a12d8bfeee98e44e81c8b3aacc1b8c5d8d0b
lorenzo <lorenzo@lorenzo.info.com>
2020-03-13 14:02:43 +0100

    code

--- src/main/java/com/reedelk/rest/component/listener/OpenApiResponse.java
+++ src/main/java/com/reedelk/rest/component/listener/OpenApiResponse.java
@@ -1,5 +1,6 @@
 package com.reedelk.rest.component.listener;
 
+import com.reedelk.runtime.api.annotation.Property;
 import com.reedelk.runtime.api.component.Implementor;
 import org.osgi.service.component.annotations.Component;
 
@@ -8,17 +9,12 @@ import static org.osgi.service.component.annotations.ServiceScope.PROTOTYPE;
 @Component(service = OpenApiResponse.class, scope = PROTOTYPE)
 public class OpenApiResponse implements Implementor {
 
+    @Property("Example")
     public String example;
+    @Property("Schema")
     public String schema;
+    @Property("Media Type")
     public String mediaType;
-
-    public String getMediaType() {
-        return mediaType;
-    }
-
-    public void setMediaType(String mediaType) {
-        this.mediaType = mediaType;
-    }
 
     public String getExample() {
         return example;
@@ -26,5 +22,21 @@ public class OpenApiResponse implements Implementor {
 
     public void setExample(String example) {
         this.example = example;
+    }
+
+    public String getSchema() {
+        return schema;
+    }
+
+    public void setSchema(String schema) {
+        this.schema = schema;
+    }
+
+    public String getMediaType() {
+        return mediaType;
+    }
+
+    public void setMediaType(String mediaType) {
+        this.mediaType = mediaType;
     }
 }
