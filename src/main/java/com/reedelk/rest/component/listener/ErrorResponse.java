0d5dfe79fa45f8e4e5599edff067d237eca7ffed
lorenzo <info@reedelk.com>
2020-05-01 10:21:58 +0200

    Working on input/output descriptors for components + attributes, WIP.

--- src/main/java/com/reedelk/rest/component/listener/ErrorResponse.java
+++ src/main/java/com/reedelk/rest/component/listener/ErrorResponse.java
@@ -18,16 +18,14 @@ public class ErrorResponse implements Implementor {
     @Property("Body")
     @Hint("error body text")
     @InitValue("#[error]")
-    @ScriptVariable(name = "error", type = Error.class)
-    @ScriptVariable(name = "context", type = FlowContext.class)
+    @ScriptSignature(arguments = {"context", "error"}, types = {FlowContext.class, Error.class})
     @Description("The body of the error response might be a static or a dynamic value.")
     private DynamicByteArray body;
 
     @Property("Status")
     @Hint("500")
     @InitValue("500")
-    @ScriptVariable(name = "error", type = Error.class)
-    @ScriptVariable(name = "context", type = FlowContext.class)
+    @ScriptSignature(arguments = {"context", "error"}, types = {FlowContext.class, Error.class})
     @Description("The status code of the error response might be a static or a dynamic value, " +
             "e.g. could be a variable defined in the flow context: <code>context.myErrorResponseStatus</code>.")
     private DynamicInteger status;
@@ -36,8 +34,7 @@ public class ErrorResponse implements Implementor {
     @TabGroup("Error Response Headers")
     @KeyName("Header Name")
     @ValueName("Header Value")
-    @ScriptVariable(name = "error", type = Error.class)
-    @ScriptVariable(name = "context", type = FlowContext.class)
+    @ScriptSignature(arguments = {"context", "error"}, types = {FlowContext.class, Error.class})
     @Description("Additional headers to be set in the HTTP error response.")
     private DynamicStringMap headers = DynamicStringMap.empty();
 
