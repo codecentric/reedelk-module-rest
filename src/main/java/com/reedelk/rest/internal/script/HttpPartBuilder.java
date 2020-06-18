6354bd3719312979b61178e70cef89535218fa04
lorenzo <info@reedelk.com>
2020-06-18 20:05:00 +0200

    code

--- src/main/java/com/reedelk/rest/internal/script/HttpPartBuilder.java
+++ src/main/java/com/reedelk/rest/internal/script/HttpPartBuilder.java
@@ -3,13 +3,15 @@ package com.reedelk.rest.internal.script;
 import com.reedelk.runtime.api.annotation.Type;
 import com.reedelk.runtime.api.annotation.TypeFunction;
 
-@Type(
-        global = true,
+@Type(global = true,
         description = "The HttpPartBuilder creates new HttpPart objects.")
 public class HttpPartBuilder {
 
     @TypeFunction(signature = "create()",
-            example = "HttpPartBuilder.create()",
+            example = "HttpPartBuilder.create()" +
+                    ".attribute('filename','my_image.png')" +
+                    ".binary(message.payload(), 'image/png')" +
+                    ".build()",
             description = "Creates a new HttpPart object.")
     public HttpPart create() {
         return new HttpPart();
