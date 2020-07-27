862945ebe15b815506f303e1b08a995c24081442
lorenzo <info@reedelk.com>
2020-07-27 17:37:45 +0200

    Open API library.

--- src/main/java/com/reedelk/rest/component/listener/openapi/v3/model/MediaTypeObject.java
+++ src/main/java/com/reedelk/rest/component/listener/openapi/v3/model/MediaTypeObject.java
@@ -5,7 +5,7 @@ import com.reedelk.runtime.api.commons.StreamUtils;
 import com.reedelk.runtime.api.component.Implementor;
 import com.reedelk.runtime.api.resource.ResourceText;
 import com.reedelk.runtime.openapi.v3.OpenApiSerializableContext;
-import com.reedelk.runtime.openapi.v3.model.ExampleReference;
+import com.reedelk.runtime.openapi.v3.model.Example;
 import com.reedelk.runtime.openapi.v3.model.Schema;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ServiceScope;
@@ -16,7 +16,7 @@ public class MediaTypeObject implements Implementor, OpenAPIModel<com.reedelk.ru
     @Property("Example")
     @WidthAuto
     @Hint("assets/data_model.json")
-    @Example("assets/data_model.json")
+    @com.reedelk.runtime.api.annotation.Example("assets/data_model.json")
     @HintBrowseFile("Select Example File ...")
     @Description("The path and name of the file to be read from the project's resources folder.")
     private ResourceText example;
@@ -25,7 +25,7 @@ public class MediaTypeObject implements Implementor, OpenAPIModel<com.reedelk.ru
     @WidthAuto
     @Hint("assets/data_model.json")
     @HintBrowseFile("Select Schema File ...")
-    @Example("assets/data_model.json")
+    @com.reedelk.runtime.api.annotation.Example("assets/data_model.json")
     @Description("The path and name of the file to be read from the project's resources folder.")
     private ResourceText schema;
 
@@ -58,7 +58,7 @@ public class MediaTypeObject implements Implementor, OpenAPIModel<com.reedelk.ru
         // Example
         if (example != null) {
             String exampleData = StreamUtils.FromString.consume(example.data());
-            mappedMediaType.setExample(new ExampleReference(exampleData));
+            mappedMediaType.setExample(new Example(exampleData));
         }
         return mappedMediaType;
     }
